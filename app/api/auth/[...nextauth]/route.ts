6364c47a107304ef0289535b13ba7fbf46f49e3e
Kongkon Bora <kbora3525@gmail.com>
2025-01-29 18:23:20 +0530

    adding endpoints

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,3 +1,4 @@
+import { prismaClient } from "@/app/lib/db";
 import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 
@@ -7,7 +8,22 @@ const handler = NextAuth({
             clientId: process.env.GOOGLE_CLIENT_ID ?? "",
             clientSecret: process.env.GOOGLE_CLIENT_SECRET ?? ""
         })
-    ]
+    ],callbacks:{
+        async signIn(params) {
+            console.log(params);
+            try{
+                await prismaClient.user.create({
+                    data:{
+                        email:"",
+                        provider:"Google"
+                    }
+                })
+            }catch(e){
+
+            }
+            return true;
+        },
+    }
 })
 
 export { handler as GET , handler as POST }
